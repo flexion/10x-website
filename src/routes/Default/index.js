1cfb98fb94de8a903793bd3599845351742cfade
Tim Plummer <tplummer@flexion.us>
2020-09-15 16:59:58 -0500

    header styles, fun loading animation

--- src/routes/Default/index.js
+++ src/routes/Default/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid, Row, Col } from "components/Grid";
 import PageList from "features/PageList";
+import Loading from "components/Loading";
 
 const Default = () => {
   return (
@@ -9,6 +10,7 @@ const Default = () => {
         <Col size={12}>
           <div style={{ paddingTop: "5vh", minHeight: "50vh" }}>
             <h1>Fund your idea</h1>
+            <Loading isLoading={true}>Not loading</Loading>
             <PageList />
           </div>
         </Col>
